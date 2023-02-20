b75662633929cbd91820b97e70622c737225d401
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-02-20 21:11:54 +0100

    began Calculator component (only buttons with digit yet)

---
+++ app/src/components/Calculator.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const label = ref('');
+    const digit = ref(-1);
+
+    const selectDigit = (value) => {
+        digit.value = value;
+    }
+
+</script>
+
+<template>
+    <div>
+        <div>
+            <h1>Calculator</h1>
+        </div>
+        <div>
+            <div>
+                <p>{{ label }}</p>
+            </div>
+            <div>
+                <button @click="selectDigit(0)">0</button>
+                <button @click="selectDigit(1)">1</button>
+                <button @click="selectDigit(2)">2</button>
+                <button @click="selectDigit(3)">3</button>
+                <button @click="selectDigit(4)">4</button>
+                <button @click="selectDigit(5)">5</button>
+                <button @click="selectDigit(6)">6</button>
+                <button @click="selectDigit(7)">7</button>
+                <button @click="selectDigit(8)">8</button>
+                <button @click="selectDigit(9)">9</button>
+            </div>
+        </div>
+        <p>{{ digit }}</p>
+    </div>
+    
+
+</template>
