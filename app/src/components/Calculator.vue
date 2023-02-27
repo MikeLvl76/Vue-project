6c25631b8038afd4dc9b2cd40916e73b2195ddd8
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-02-27 23:44:15 +0100

    front done for the moment

--- app/src/components/Calculator.vue
+++ app/src/components/Calculator.vue
@@ -20,7 +20,7 @@ const selectOperator = (op) => {
 const updateCalculation = (event) => {
     if (event === 'C') label.value = '';
     else if (event === 'DEL') label.value = label.value.substring(0, label.value.length - 1);
-    else if (event === 'OK') {
+    else if (event === '=') {
         label.value = calculate(label.value);
     }
 }
@@ -29,39 +29,31 @@ const updateCalculation = (event) => {
 
 <template>
     <div>
-        <div class="bg-gray-300 w-fit rounded-full mt-5 mx-auto px-2 py-2">
-            <h1 class="text-2xl text-center">Calculator</h1>
-        </div>
-        <div class="border border-1 border-gray-700 w-96 rounded-lg px-2 py-2 mx-auto mt-5">
+        <div class="border border-1 border-gray-700 w-64 rounded-lg px-2 py-2 mx-auto mt-5">
             <div class="bg-gray-300 w-fit rounded-lg w-full h-12 mx-auto px-2 py-2">
-                <p>{{ label }}</p>
+                <p class="text-right">{{ label }}</p>
             </div>
-            <div class="flex flex-row mt-5">
-                <button class="bg-gray-900 text-white rounded-lg w-full text-center"
-                    @click="updateCalculation('C')">C</button>
-                <button class="bg-red-500 text-white rounded-lg w-full text-center"
-                    @click="updateCalculation('DEL')">DEL</button>
-                <button class="bg-green-500 text-white rounded-lg w-full text-center"
-                    @click="updateCalculation('OK')">OK</button>
+            <div class="flex flex-row space-x-5 mt-5 justify-center">
+                <div class="grid grid-cols-3 gap-2">
+                    <li v-for="i in 9" class="list-none">
+                        <button class="bg-gray-900 text-white px-2 py-2 w-full rounded-lg" @click="selectDigit(i)">{{ i
+                        }}</button>
+                    </li>
+                    <button class="bg-gray-900 text-white px-2 py-2 rounded-lg w-full" @click="selectDigit(0)">0</button>
+                    <button class="bg-gray-500 text-white px-2 py-2 rounded-lg w-full"
+                        @click="updateCalculation('C')">C</button>
+                    <button class="bg-red-500 text-white rounded-lg px-2 py-2 w-fit"
+                        @click="updateCalculation('DEL')">DEL</button>
+                </div>
+                <div class="flex flex-col">
+                    <button class="bg-blue-500 text-white px-1 py-1 rounded-full w-8 mb-1 mt-1" @click="selectOperator('+')">+</button>
+                    <button class="bg-blue-500 text-white px-1 py-1 rounded-full w-8 mb-1 mt-1" @click="selectOperator('-')">-</button>
+                    <button class="bg-blue-500 text-white px-1 py-1 rounded-full w-8 mb-1 mt-1" @click="selectOperator('*')">x</button>
+                    <button class="bg-blue-500 text-white px-1 py-1 rounded-full w-8 mb-1 mt-1" @click="selectOperator('/')">/</button>
+                    <button class="bg-green-500 text-white rounded-full w-8"
+                        @click="updateCalculation('=')">=</button>
+                </div>
             </div>
-            <div>
-                <button @click="selectDigit(0)">0</button>
-                <button @click="selectDigit(1)">1</button>
-                <button @click="selectDigit(2)">2</button>
-                <button @click="selectDigit(3)">3</button>
-                <button @click="selectDigit(4)">4</button>
-                <button @click="selectDigit(5)">5</button>
-                <button @click="selectDigit(6)">6</button>
-                <button @click="selectDigit(7)">7</button>
-                <button @click="selectDigit(8)">8</button>
-                <button @click="selectDigit(9)">9</button>
-            </div>
-            <div>
-                <button @click="selectOperator('+')">+</button>
-                <button @click="selectOperator('-')">-</button>
-                <button @click="selectOperator('*')">x</button>
-                <button @click="selectOperator('/')">/</button>
-            </div>
-
         </div>
-</div></template>
+    </div>
+</template>
